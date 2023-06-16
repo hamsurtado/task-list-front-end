c8614640ed630934907e45e4ba02d803cab2f0c4
Trang Tran <trantrang181197@gmail.com>
2023-06-16 14:11:05 -0700

    toggle complete done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import { useState } from 'react';
 
 const TASKS = [
   {
@@ -16,13 +17,30 @@ const TASKS = [
 ];
 
 const App = () => {
+  const [tasks, setTasks] = useState(TASKS);
+
+  // Toggle complete feature
+  const updateComplete = (id) => {
+    const newTasks = tasks.map((task) => {
+      if (task.id === id) {
+        const updateTask = {...task};
+        updateTask.isComplete = !task.isComplete;
+        return updateTask;
+      } else {
+        return task;
+      }
+    });
+    setTasks(newTasks);
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>{<TaskList tasks={tasks} updateComplete={updateComplete}/>}</div>
       </main>
     </div>
   );
